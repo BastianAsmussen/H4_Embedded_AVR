89132f930fecde0deb25cf43b4343abc5ee9a8da
Bastian Asmussen <bastian@asmussen.tech>
2025-01-23 17:32:17 +0100

    chore: begin to implement HAL

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod hal;
+
+use core::{hint::black_box, panic::PanicInfo};
 
 const DDRB: *mut u8 = 0x24 as *mut u8;
 const PORTB: *mut u8 = 0x25 as *mut u8;
+
+fn sleep() {
+    for i in 0..100_000 {
+        black_box(i);
+    }
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn main() {
     unsafe {
         core::ptr::write_volatile(DDRB, core::ptr::read_volatile(DDRB) | 0b0000_1111);
         core::ptr::write_volatile(PORTB, 0b0000_1010);
+    }
+
+    loop {
+        sleep();
     }
 }
 
